c050df1e4226fce5ebd1c56d429d0d5acc43b2cf
FredrikEkmark <fredrik.ekmark@gmail.com>
2023-04-14 21:50:45 +0200

    Added error handleing

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -5,7 +5,13 @@ const prisma = new PrismaClient()
 
 const handler: NextApiHandler = async (req, res) => {
   const users = await prisma.user.findMany()
-  res.status(200).json(users)
+  switch (res.statusCode) {
+    case 200:
+      res.status(200).json(users)
+      break
+    case 500:
+      res.status(500).send("Internal Error")
+  }
 }
 
 export default handler
